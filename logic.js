b60fb9b3fe23caf9f3e1be395a0e6a5f2c2ad11e
Vladimir Espinoza <vladi981022@gmail.com>
2023-10-14 09:45:15 -0700

    Se  agregaron funciones , manejo de arrays y objetos

--- logic.js
+++ logic.js
@@ -1,9 +1,41 @@
-let array = [25,38,35]
-let max = 0
+let array = [25,38,35,4,24,32];
+let max = 0;
+
+let myCar = new Object();
+myCar.type = "sport";
+myCar.make = "ford";
+myCar.model = "mustang";
+myCar.year = 1969;
+
+let fernanda = {
+    eyes: 'blue',
+    skin: 'white',
+    height: 1.6
+
+}
+
 
 for(let i = 0; i < array.length; i++){
     if(array[i]>max){
         max = array[i]
     }
 }
-alert(max)
+
+function showArray(array){
+    array.forEach(element => {
+        console.log(element)
+    });
+}
+
+const showObject = (object) =>{
+    for (const value in object) {
+        if (Object.hasOwnProperty.call(object, value)) {
+            const element = object[value];
+            console.log(Object.hasOwn('eyes'));
+            console.log(element)
+        }
+    }
+}
+showObject(fernanda)
+// showArray(array)
+
